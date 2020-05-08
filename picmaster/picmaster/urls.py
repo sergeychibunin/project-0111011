e5dc25430097bb4e29ce7affe8981d121d394e00
Sergey Chibunin <chibunin1988@gmail.com>
2020-05-08 22:07:19 +0500

    Add the pic-endpoint

--- picmaster/picmaster/urls.py
+++ picmaster/picmaster/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from picmaster.pmapp import views
+from pmapp import views
 
 router = routers.DefaultRouter()
-#router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
+    path('pic/', views.upload),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
